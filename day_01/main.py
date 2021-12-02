38dfed34f82b89c427f0083a732a1b33f9b2f464
lac-anakagawa <anakagawa@clippers.com>
2021-12-01 19:25:05 -0800

    :art: scaffold increases func

--- day_01/main.py
+++ day_01/main.py
@@ -1,15 +1,15 @@
 def get_arr_from_txt(filepath: str) -> list:
     with open(filepath) as f:
         ret = [int(line) for line in f]
+
+    num_records = len(ret)
+    print(f"There are {num_records} records in the input")
+
     return ret
 
 
-def get_num_increases(filepath: str) -> int:
-    depths = get_arr_from_txt(filepath)
+def get_num_increases(depths: list) -> int:
     num_depths = len(depths)
-
-    print(f"There are {num_depths} depths in the input")
-
     if num_depths == 0 or num_depths == 1:
         return 0
 
@@ -22,5 +22,4 @@ def get_num_increases(filepath: str) -> int:
 
 if __name__ == "__main__":
     increases = get_num_increases("./input.txt")
-    print(f"There are {increases} decreases in depth in the input")
-    
+    print(f"There are {increases} increases in depth in the input")
