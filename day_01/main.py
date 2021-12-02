a9955d48ab0585cc4a156f3a523ddcc991add76c
lac-anakagawa <anakagawa@clippers.com>
2021-12-01 19:41:30 -0800

    :sparkles: complete part 2

--- day_01/main.py
+++ day_01/main.py
@@ -8,18 +8,34 @@ def get_arr_from_txt(filepath: str) -> list:
     return ret
 
 
-def get_num_increases(depths: list) -> int:
+def get_three_sliding_window_sums(depths: list, window_size:int=3) -> list:
     num_depths = len(depths)
+    if num_depths < window_size:
+        return []
+
+    ret_sums = list()
+    for i in range(num_depths - window_size + 1):
+        ret_sums.append(sum(depths[i:i+window_size]))
+
+    return ret_sums
+
+
+def get_num_increases(lst_depths: list) -> int:
+    num_depths = len(lst_depths)
     if num_depths == 0 or num_depths == 1:
         return 0
 
     ctx = 0
     for i in range(num_depths - 1):
-        ctx += (1 if depths[i] < depths[i+1] else 0)
+        ctx += (1 if lst_depths[i] < lst_depths[i+1] else 0)
 
     return ctx
 
 
 if __name__ == "__main__":
-    increases = get_num_increases("./input.txt")
+    depths = get_arr_from_txt("./input.txt")
+    increases = get_num_increases(depths)
     print(f"There are {increases} increases in depth in the input")
+    sliding_window_sums = get_three_sliding_window_sums(depths)
+    window_increases = get_num_increases(sliding_window_sums)
+    print(f"There are {window_increases} sliding window increases")
